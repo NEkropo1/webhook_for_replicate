a8ab23b96bbfd29c32432f56c2d12a1dc0015015
Paul Savchuk <nekrjkeee@gmail.com>
2023-07-28 13:42:51 +0300

    add refactored pushover code for main project

---
+++ pushover/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='pushover',
+        version='0.1',
+        url='https://github.com/Wyattjoh/pushover',
+        py_modules=['pushover'],
+        scripts=['pushover'],
+        author='Wyatt Johnson',
+        author_email='wyattjoh@gmail.com',
+        )
