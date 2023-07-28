72210759ceed67cc9f867ae278cf0deaddc9fccf
Paul Savchuk <nekrjkeee@gmail.com>
2023-07-28 11:32:53 +0300

    changed logging to logger

--- start_server.py
+++ start_server.py
@@ -1,10 +1,24 @@
+import logging
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+logger = logging.getLogger("webhook_logger")
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(log_format)
+
+# Add the handler to the logger
+logger.addHandler(console_handler)
 
 
 @app.post("/your-webhook")
 async def receive_webhook(payload: dict):
     # Process the payload received from Replicate webhook here
-    print("Received webhook payload:", payload)
+    logger.info("Received webhook payload: %s", payload)
     return {"message": "Webhook received successfully!"}
