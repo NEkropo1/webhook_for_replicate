09cc9d20636edf7d4fd6ce2be35e7edc8633fa05
Paul Savchuk <nekrjkeee@gmail.com>
2023-07-28 13:26:47 +0300

    add pushover handler and refactored endpoints + ids

---
+++ clients.py
@@ -0,0 +1,12 @@
+class ClientsDB:
+    def __init__(self):
+        self.clients = {}
+
+    def __contains__(self, item):
+        return item in self.clients
+
+    def add_clients(self, payload: dict = None) -> bool:
+        device_id = payload.get("device_id")
+        _id = payload.get("id")
+        self.clients[_id] = device_id
+        return True
--- start_server.py
+++ start_server.py
@@ -1,7 +1,14 @@
 import logging
+import os
 
 from fastapi import FastAPI
+from pushover.pushover import Pushover
 
+from clients import ClientsDB
+
+po_token = os.getenv("PUSH_APP_TOKEN")
+clients = ClientsDB()
+po = Pushover(po_token)
 app = FastAPI()
 
 logger = logging.getLogger("webhook_logger")
@@ -13,12 +20,29 @@ console_handler.setLevel(logging.INFO)
 log_format = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 console_handler.setFormatter(log_format)
 
-# Add the handler to the logger
 logger.addHandler(console_handler)
 
 
-@app.post("/your-webhook")
-async def receive_webhook(payload: dict):
-    # Process the payload received from Replicate webhook here
+@app.post("/receive_client_ids")
+def receive_client_ids(payload: dict):
+    try:
+        clients.add_clients(payload)
+        return {"message": "Id's received successfully!"}
+    except Exception as e:
+        logger.error("Failed to add clients: %s", str(e))
+        return {"message": "Unexpected exception: " + str(e)}
+
+
+@app.post("/receive_prediction_results")
+def receive_prediction_results(payload: dict):
     logger.info("Received webhook payload: %s", payload)
+    _id = payload.get("id")
+    if _id and _id in clients.clients:
+        user_token = clients.clients[_id]
+        po.user(user_token)
+        msg = po.msg(f"You prediction {_id} is complete!")
+        msg.set("title", "Nona me dig it all prediction completed!")
+        po.send(msg)
+    else:
+        logger.warning("ID %s not found in clients DB.", _id)
     return {"message": "Webhook received successfully!"}
